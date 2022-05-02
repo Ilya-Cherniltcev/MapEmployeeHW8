2f3d981c0e457729c66ad23d77c9f2767ac480b9
Ilya Cherniltsev <cherniltcev@mail.ru>
2022-05-02 12:04:38 +0500

    first commit

---
+++ src/main/java/pro/sky/exceptionsemployeehw5/ExceptionsEmployeeHw5Application.java
@@ -0,0 +1,13 @@
+package pro.sky.exceptionsemployeehw5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsEmployeeHw5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionsEmployeeHw5Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/exceptionsemployeehw5/ExceptionsEmployeeHw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.exceptionsemployeehw5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsEmployeeHw5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
